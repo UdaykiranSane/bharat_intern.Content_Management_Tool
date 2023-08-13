ad7e159ecb9801770ac86eb34a928923049984eb
S.Uday kiran <137673416+UdaykiranSane@users.noreply.github.com>
2023-08-13 10:48:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-
-io.on('connection', (socket) => 
-{
-  console.log('A user connected');
-
-  socket.on('send_message', (data) => 
-  {
-    io.emit('receive_message', data);
-  });
-
-  socket.on('disconnect', () => 
-  {
-    console.log('A user disconnected');
-  });
-});
-
-
-const PORT = 3000;
-http.listen(PORT, () => 
-{
-  console.log(`Server is running on port ${PORT}`);
-});
+var content = {
+    title: "",
+    body: "",
+    author: "",
+    publishDate: ""
+  };
+  
+  // Function to create new content
+  function createContent(title, body, author, publishDate) {
+    var newContent = Object.create(content);
+    newContent.title = title;
+    newContent.body = body;
+    newContent.author = author;
+    newContent.publishDate = publishDate;
+    return newContent;
+  }
+  
+  // Example usage
+  var myContent = createContent("Sample Title", "This is the body of the content.", "John Doe", "2023-07-10");
+  console.log(myContent);
+  
+  // Function to update content
+  function updateContent(existingContent, newTitle, newBody, newAuthor, newPublishDate) {
+    existingContent.title = newTitle;
+    existingContent.body = newBody;
+    existingContent.author = newAuthor;
+    existingContent.publishDate = newPublishDate;
+  }
+  
+  // Example usage
+  updateContent(myContent, "Updated Title", "This is the updated body of the content.", "Jane Smith", "2023-07-11");
+  console.log(myContent);
